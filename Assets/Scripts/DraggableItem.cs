da0ff829b234036d687deb962d278eba36f2e2e4
maximtsai <maximtsai@yahoo.com>
2025-01-03 12:26:49 -0800

    unity scene update

--- Assets/Scripts/DraggableItem.cs
+++ Assets/Scripts/DraggableItem.cs
@@ -1,13 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class DraggableItem : MonoBehaviour
 {
+    // Vector3 mousePositionOffset;
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        // capture mouse position and return WorldPoint
+        Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        newPos.z = 0f;
+        return newPos;
+    }
+
+    private void OnMouseDown()
+    {
+        // capture mouse offset pos
+        // mousePositionOffset = gameObject.transform.position - GetMouseWorldPosition();
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPosition();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start");
     }
 
     // Update is called once per frame
