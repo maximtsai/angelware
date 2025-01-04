e95403ee9aec30cb88b9b06adada97555cc40836
Alisa <alissaren98@gmail.com>
2025-01-03 19:41:36 -0800

    Copied start menu stuff from eckspelled :P

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public GameObject blackScreen;
+    private bool clicked = false;
+
+    public void StartButtonClicked()
+    {
+        if (!clicked) {
+            clicked = true;
+            blackScreen.SetActive(true);
+            blackScreen.GetComponent<FadeScreen>().StartFadeScreen();
+            Invoke("LoadMainScene", 0.5f);
+        }
+    }
+
+    void LoadMainScene() {
+        SceneManager.LoadScene("MainScene");
+    }
+}
