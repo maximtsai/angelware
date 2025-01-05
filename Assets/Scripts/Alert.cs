08bae3ec1750dc2c12c776a99cc822bac5e9f34c
alizawren <alissaren98@gmail.com>
2025-01-05 11:30:50 -0800

    Alert updates

--- Assets/Scripts/Alert.cs
+++ Assets/Scripts/Alert.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Image))]
 public class Alert : MonoBehaviour
 {
+    public Sprite idleSprite;
     public AnimationClip alertEnter;
     public Sprite windowSprite;
 
@@ -13,37 +14,60 @@ public class Alert : MonoBehaviour
     private Animation animationComponent;
     private UISpritesAnimation uiSpritesAnimation;
     private GameObject okButton;
+    private GameObject exclamation;
 
-    private enum Phase { WAITING, WINDOW }
-    private Phase phase = Phase.WAITING;
+    private enum Phase { IDLE, WAITING, WINDOW }
+    private Phase phase = Phase.IDLE;
+
+    private float timer = 0.0f;
 
     void Start()
     {
         image = GetComponent<Image>();
+        if (idleSprite != null) {
+            image.sprite = idleSprite;
+        }
         // image.color = new Color(1, 1, 1, 0);
         
         animationComponent = GetComponent<Animation>();
         
         uiSpritesAnimation = GetComponent<UISpritesAnimation>();
-        uiSpritesAnimation.currentSpriteSequence = 0;
-        uiSpritesAnimation.playOnAwake = true;
-        uiSpritesAnimation.loop = true;
 
         okButton = GameObject.Find("OKButton");
         okButton.SetActive(false);
         okButton.GetComponent<AlertOKButton>().enabled = false;
 
-        phase = Phase.WAITING;
-        // Invoke("Appear", 1f);
+        exclamation = GameObject.Find("Exclamation");
+        exclamation.SetActive(false);
+
+        phase = Phase.IDLE;
     }
 
-    // public void Appear() {
-    //     animationComponent.clip = alertEnter;
-    //     animationComponent.Play();
-    // }
+    void Update() 
+    {
+        if (phase == Phase.WAITING) {
+            timer += Time.deltaTime;
+            if (timer > 1.0f) {
+                timer = 0f;
+                if (exclamation.activeSelf) {
+                    exclamation.SetActive(false);
+                }
+                else {
+                    exclamation.SetActive(true);
+                }
+            }
+        }
+    }
+
+    public void TriggerAlert() {
+        phase = Phase.WAITING;
+        uiSpritesAnimation.currentSpriteSequence = 0;
+        uiSpritesAnimation.Play();
+    }
 
     void OnMouseDown() {
         if (phase == Phase.WAITING) {
+            uiSpritesAnimation.Stop();
             phase = Phase.WINDOW;
             image.sprite = windowSprite;
             okButton.SetActive(true);
