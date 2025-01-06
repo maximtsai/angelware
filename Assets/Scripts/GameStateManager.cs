1f3d9e2be1b5b601a8fc4b1b6b49508fd9b21d46
alizawren <alissaren98@gmail.com>
2025-01-05 21:16:52 -0800

    Small changes

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -2,10 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEditor.SearchService;
-using UnityEditorInternal;
 using UnityEngine.SceneManagement;
-using System.Diagnostics.Tracing;
 
 public class GameStateManager : MonoBehaviour
 {
